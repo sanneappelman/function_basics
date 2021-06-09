69046eafdd40b64677244d9615294628bda32b98
sanneappelman <84176938+sanneappelman@users.noreply.github.com>
2021-06-09 19:27:49 +0200

    Add files via upload

---
+++ Opdracht_6/README.md
@@ -0,0 +1,14 @@
+# Function basics
+
+## What functions are
+
+    - serie of steps
+    - optional: name
+    - you can "call" a function
+
+Mental model of a function
+
+## why use functions
+
+    -not having to repeat yourself
+    -a series of steps have a name
---
+++ Opdracht_6/scrips.js
@@ -0,0 +1,23 @@
+
+const pizzaMaking = function () {
+    console.log ("maak het deeg");
+    console.log ("maak een bodem van het deeg");
+    console.log ("Doe tomatensaus over het deeg heen");
+    console.log ("Leg plakjes mozzarella op je pizza")
+    console.log ("Doe de pizza in de oven tot hij mooi bruin is");
+}
+
+const sushiMaking = function () {
+    console.log ("kook de rijst volgens de verpakking");
+    console.log ("snijd de zalm in stukjes");
+    console.log ("leg een zeewiervel op het aanrecht")
+    console.log ("plet een lepel rijst op het zeewiervel")
+    console.log ("doe hier de plakjes zalm overheen")
+    console.log ("rol de sushi op en snijdt hem in stukjes")
+}
+
+sushiMaking ();
+sushiMaking ();
+sushiMaking ();
+pizzaMaking ();
+pizzaMaking ();
